b4493f5c03f909a15df33b2c69fa7f2f9cbeade9
hybp <hybp@hku.hk>
2023-04-20 19:26:37 +0800

    Create repository

---
+++ src/GameGUI.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameGUI {
+    JFrame frame = new JFrame();
+
+    public static void main(String[] args){
+        GameGUI gui = new GameGUI();
+        gui.go();
+    }
+    public void go() {
+
+    }
+    class CardClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+
+        }
+    }
+    class someListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+
+        }
+    }
+    public void
+}
+
+
+class MyCardPanel extends JPanel {
+
+}
